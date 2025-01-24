9dc2bdcf1250130f4e77e434ad438d1bc6cbd500
nurlybek <orozovnurlybek@gmail.com>
2025-01-24 15:25:28 +0600

    Импортировал контейнеры и добавил функцию переключения между ними

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import './App.css'
+import Movie from "./containers/Movie/Movie.tsx";
+import Jokes from "./containers/Jokes/Jokes.tsx";
+import {useState} from "react";
 
 const App = () => {
+    const [page, setPage] = useState<boolean>(false);
 
+    return (
+
+        <div className='container mt-3'>
+            <button className='btn btn-light mb-2' onClick={() => setPage(!page)}>Change page</button>
+            {page ?  <Movie/> : <Jokes/>}
+
+        </div>
+    )
 };
 
 export default App
